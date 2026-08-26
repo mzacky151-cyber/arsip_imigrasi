c05ed2cee101a527e7a90d1ab848ab044753a6aa
mzacky151-cyber <mzacky151@gmail.com>
2026-08-27 01:46:02 +0700

    Update sidebar.php

--- template/sidebar.php
+++ template/sidebar.php
@@ -7,7 +7,7 @@ $folder = basename(dirname($_SERVER['PHP_SELF']));
 
     <div class="sidebar-header">
 
-        <img src="../assets/img/logo-imigrasi.png" alt="Logo">
+        <img src="/assets/img/logo-imigrasi.png" alt="Logo">
 
         <h2>ARSIPIN</h2>
 
@@ -17,49 +17,49 @@ $folder = basename(dirname($_SERVER['PHP_SELF']));
 
     <div class="sidebar-menu">
 
-        <a href="../dashboard/index.php"
+        <a href="/dashboard/index.php"
             class="<?= ($folder=="dashboard") ? "active" : ""; ?>">
             Dashboard
             </a>
        
-            <a href="../data_arsip/index.php"
+            <a href="/data_arsip/index.php"
             class="<?= ($folder=="data_arsip") ? "active" : ""; ?>">
             Data Arsip
             </a>
 
             <?php if (punyaAkses(["admin"])) : ?>
-            <a href="../master_lokasi/index.php"
+            <a href="/master_lokasi/index.php"
             class="<?= ($folder=="master_lokasi") ? "active" : ""; ?>">
             Master Lokasi Rak
             </a>
             <?php endif; ?>
 
         
-            <a href="../visualisasi_rak/index.php"
+            <a href="/visualisasi_rak/index.php"
             class="<?= ($folder=="visualisasi_rak") ? "active" : ""; ?>">
             Visualisasi Rak
             </a>
         
-            <a href="../peminjaman/index.php"
+            <a href="/peminjaman/index.php"
             class="<?= ($folder=="peminjaman") ? "active" : ""; ?>">
             Peminjaman
             </a>
             
             <?php if (punyaAkses(["admin"])) : ?>
-            <a href="../user/index.php"
+            <a href="/user/index.php"
             class="<?= $folder=="user" ? "active" : ""; ?>">
             Manajemen User
             </a>
             <?php endif; ?>
             
             <?php if (punyaAkses(["admin"])) : ?>
-            <a href="../laporan/index.php"
+            <a href="/laporan/index.php"
             class="<?= ($folder=="laporan") ? "active" : ""; ?>">
             Laporan
             </a>
             <?php endif; ?>
        
-            <a href="../logout/logout.php">
+            <a href="/logout/logout.php">
             Logout
             </a>
 
